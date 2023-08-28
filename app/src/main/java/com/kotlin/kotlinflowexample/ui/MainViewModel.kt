a24a4d6183cfd3ad972a6947444d78a64361c371
RohanRamola <rohanramola63@gmail.com>
2023-08-28 20:38:32 +0530

    Added Flow operators

--- app/src/main/java/com/kotlin/kotlinflowexample/ui/MainViewModel.kt
+++ app/src/main/java/com/kotlin/kotlinflowexample/ui/MainViewModel.kt
@@ -3,25 +3,29 @@ package com.kotlin.kotlinflowexample.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.fold
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.reduce
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     //Producer
-    val countDown = flow<Int> {
+    val countDown = flow {
 
         val startValue = 5
         var currentValue = startValue
         emit(startValue)
 
-        while (currentValue > 0){
+        while (currentValue > 0) {
             delay(1000)
             currentValue--
             emit(currentValue)
@@ -29,35 +33,62 @@ class MainViewModel : ViewModel() {
     }
 
     init {
-        collectFlow()
+        collectRoutine()
     }
 
-    private fun collectFlow(){
+    private fun collectFlow() {
 
         //Consumer
         viewModelScope.launch {
-           val result = countDown
-                .filter {time ->
+            val result = countDown
+                .filter { time ->
                     time % 2 == 0
                 }
-                .map {time ->
+                .map { time ->
                     time * time
                 }.count()
             println(result)
         }
 
         viewModelScope.launch {
-            val result = countDown.reduce{ accumulator, value ->
+            val result = countDown.reduce { accumulator, value ->
                 accumulator + value
             }
             println(result)
         }
 
         viewModelScope.launch {
-            val result = countDown.fold(20){ accumulator, value ->
+            val result = countDown.fold(20) { accumulator, value ->
                 accumulator + value
             }
             println(result)
         }
+    }
+
+    private fun collectRoutine() {
+
+        val flow = flow {
+            delay(1000)
+            emit("Eat")
+
+            delay(1000)
+            emit("work")
+
+            delay(1000)
+            emit("gym")
+
+        }
+
+        //buffer, conflate, collectLatest
+        viewModelScope.launch {
+            flow.onEach {
+                println("Flow: $it time")
+            }.buffer()
+                .collect {
+                    delay(1500)
+                    println("Flow: $it done")
+                }
+        }
+
     }
 }
